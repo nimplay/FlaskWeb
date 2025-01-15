5ccbb7e0f7f7820b430cf5f0ce345d234a4e4cec
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2025-01-15 16:48:33 -0400

    add html

--- tutorialone.py
+++ tutorialone.py
@@ -1,18 +1,18 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello My friend <h1>PEPE</h1>"
+    return render_template("index.html")
 
-@app.route("/<name>")
+""" @app.route("/<name>")
 def user(name):
     return f"hello {name}"
 
 @app.route("/admin")
 def admin():
-    return redirect(url_for("user", name="Admin!"))
+    return redirect(url_for("user", name="Admin!")) """
 
 
 if __name__ == "__main__":
